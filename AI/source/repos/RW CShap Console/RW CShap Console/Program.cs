221d3761285c40c3a67f93841f30b2f33ed403d6
ydengca <hdcydeng@gmail.com>
2018-07-29 21:28:56 -0400

    Part 2
    https://www.youtube.com/watch?v=6QcHJ33YrYk&t=338s

---
+++ AI/source/repos/RW CShap Console/RW CShap Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RW_CShap_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name:");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name:");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, {1}",  FirstName, LastName);
+            Console.Read();
+        }
+    }
+}
